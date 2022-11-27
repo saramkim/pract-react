e462eee9b4c7264ab56240659821aa628a46d07b
saram <yujun1018@naver.com>
2022-11-27 15:33:56 +0900

    feat: 끝말잇기 구현

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -1,7 +1,37 @@
+const { useState, useRef } = require('react');
 const React = require('react');
 
 const WordRelay = () => {
-  return <div>WordRelay</div>;
+  const [word, setWord] = useState('김사람');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
+
+  const CORRECT = '딩동댕';
+  const WRONG = '땡';
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult(CORRECT);
+      setWord(value);
+      setValue('');
+    } else {
+      setResult(WRONG);
+    }
+    inputRef.current.focus();
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputRef} onChange={(e) => setValue(e.target.value)} value={value} />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
 };
 
 module.exports = WordRelay;
